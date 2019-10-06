bd3931d84dd7ff10c100e52d724b23da50804c47
Salim <33899208+Heffar@users.noreply.github.com>
2019-10-06 16:30:03 +0200

    Initial configuration.

--- ColoringAppOpenCv/Main.cpp
+++ ColoringAppOpenCv/Main.cpp
@@ -1,7 +1,31 @@
+#include <opencv2/highgui.hpp>
+#include <Windows.h>
 #include <iostream>
 
+using namespace std;
+using namespace cv;
+
 int main() {
-	printf("Hello World!");
-	
+	Mat image;
+	VideoCapture camera;
+	camera.set(CAP_PROP_FRAME_WIDTH, 640);
+	camera.set(CAP_PROP_FRAME_WIDTH, 480);
+	camera.open(0);
+	char c;
+
+	if (!camera.isOpened()){
+		cout << "Your camera is off";
+		return -1;
+	}
+
+	while (1) {
+		camera >> image;
+		namedWindow("Camera", 1);
+
+		imshow("Camera", image);
+
+		c = (char)waitKey(1);
+		if (c == 27) break;
+	}
 	return 0;
 }
